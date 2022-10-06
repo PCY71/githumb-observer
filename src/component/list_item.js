df5f5229c4dfb7b369b026973c160ff7fc569a56
PCY71 <skypcy71@gmail.com>
2022-10-06 14:16:53 +0900

    css 2차 정리

--- src/component/list_item.js
+++ src/component/list_item.js
@@ -3,6 +3,24 @@ import styled from 'styled-components';
 import TokenInfo from '../tokenInfo';
 
 function ListItem({ item, ticker }) {
+    let color = '#000000'
+
+    if (item?.chg_Amt > 0) {
+        color = '#f75467'
+    } else if (item?.chg_Amt < 0) {
+        color = '#4386f9'
+    }
+
+    const num_trans = (num = '0') => {
+        if (num.includes('.')) {
+            const [int, dec] = num.split('.');
+            num = int + '.' + dec.slice(0, 2);
+        }
+
+        const num_trans_reg = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g
+        return num.replace(num_trans_reg, ',')
+    }
+
     return (
         <Table>
             <colgroup>
@@ -15,30 +33,38 @@ function ListItem({ item, ticker }) {
             <tbody className="coin_list">
                 <tr className="coin">
                     <td>
-                        <div className="sort_coin_box">
+                        <CoinBox>
                             <p>
                                 <strong>{TokenInfo[ticker].name}</strong>
                                 <span className="sort_coin">{ticker}/KRW</span>
                             </p>
-                        </div>
+                        </CoinBox>
                     </td>
                     <td>
-                        <div className="sort_real_box">
-                            <strong>{item?.closing_price}원</strong>
-                        </div>
-                    </td>
-                    <td className="rate">
-                        <div>
-                            {item?.chg_Amt ? item.chg_Amt : 0} 원({item?.chg_Rate ? item.chg_Rate : 0}%)
-                        </div>
+                        <RealBox>
+                            <strong>{num_trans(item?.closing_price)}원</strong>
+                        </RealBox>
                     </td>
                     <td>
-                        <span className="assettotal">
-                            {item?.prev_closing_price}
-                        </span>
+                        <Rate color={color}>
+                            {item?.chg_Amt > 0 ? '+' : ''} {item?.chg_Amt ? num_trans(item.chg_Amt) : 0} 원( {item?.chg_Rate > 0 ? '+' : ''} {item?.chg_Rate ? item.chg_Rate : 0}%)
+                            {item?.chg_Amt
+                                ? item?.chg_Amt > 0
+                                    ? <i class="fa fa-caret-up"></i>
+                                    : <i class="fa fa-caret-down"></i>
+                                : <></>}
+
+                        </Rate>
                     </td>
                     <td>
-                        <span>≈ {item?.acc_trade_value_24H} 원</span>
+                        <PrevPrice>
+                            {num_trans(item?.prev_closing_price)} 원
+                        </PrevPrice>
+                    </td>
+                    <td>
+                        <CoinAsset>
+                            ≈ {num_trans(item?.acc_trade_value_24H)} 원
+                        </CoinAsset>
                     </td>
                 </tr>
             </tbody>
@@ -55,4 +81,30 @@ td{
     border-bottom: 1px solid #d3d3d3;
     padding: 10px;
 }
+i{
+    margin-left:5px;
+}
+`
+const CoinBox = styled.div`
+p{
+    display: flex;
+    flex-direction: column;
+    span{
+        font-size: small;
+    }
+}
+`
+const RealBox = styled.div`
+float: right;
+
+`
+const Rate = styled.div`
+float:right;
+color : ${(props) => props.color}
+`
+const PrevPrice = styled.span`
+float:right;
+`
+const CoinAsset = styled.span`
+float:right;
 `
